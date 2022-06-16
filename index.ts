0a283266035b852225276c0c2cd0d46f4819b39e
Antonios Papadakis <bdpaboi123@gmail.com>
2022-06-16 10:42:40 -0500

    Express Hello World

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-// CODE HERE
+import express from "express";
+const app = express();
+const port: Number = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
