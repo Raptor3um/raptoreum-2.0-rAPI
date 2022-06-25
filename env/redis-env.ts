fbfe754ea9d1eef4992469c1d49b4c0e39a712f1
Antonios Papadakis <bdpaboi123@gmail.com>
2022-06-24 22:22:46 -0500

    Add Number of Blocks To Cache Property to Redis ENV File

--- env/redis-env.ts
+++ env/redis-env.ts
@@ -3,7 +3,8 @@ import { EnvType, load } from 'ts-dotenv';
 export type Env = EnvType<typeof schema>;
  
 export const schema = {
-    REDIS_HOST: String
+    REDIS_HOST: String,
+    LATEST_BLOCKS_CACHE_NUM: Number,
 };
  
 export const env: Env = load(schema, "env/redis.env");
